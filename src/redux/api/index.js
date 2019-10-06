23829bfb8a293797dce7fad58f111c2365708a2f
juan.ortiz <juan.ortiz@digitalonus.com>
2019-10-06 10:19:33 -0500

    Add api function file

---
+++ src/redux/api/index.js
@@ -0,0 +1,14 @@
+const baseUrl =
+  "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices";
+
+export default (url, method, body, headers) =>
+  fetch(baseUrl + url, {
+    method,
+    ...body,
+    headers: {
+      "x-rapidapi-host":
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      "x-rapidapi-key": "c3e14d75a3msh2a179812f3c3fb8p1d23b4jsn88ced071135a",
+      ...headers
+    }
+  });
--- src/redux/sagas/flights.js
+++ src/redux/sagas/flights.js
@@ -5,10 +5,15 @@ import {
   GET_LOCATIONS_COMPLETE,
   GET_LOCATIONS_ERROR
 } from "../../consts/actionTypes";
+import apiCall from '../api';
 
 export function* getLocations({ payload }) {
   try {
-    // const results = yield call(apiCall, `&s=${payload.movieName}`, null, null, 'GET');
+    const { country, currency, locale, query } = payload;
+    const url = `/autosuggest/v1.0/${country}/${currency}/${locale}/?query=${query}`;
+    const results = yield call(apiCall, url, 'GET');
+
+    console.log(results);
     yield put({ type: GET_LOCATIONS_COMPLETE, results: [] });
   } catch (error) {
     yield put({ type: GET_LOCATIONS_ERROR, error });
